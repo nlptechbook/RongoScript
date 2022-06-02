f0032fcab2ecff313c9892e6f55bd4b6f53227d6
nlptechbook <60931109+nlptechbook@users.noreply.github.com>
2022-06-02 10:40:56 -0400

    Update rurongobot.py

--- RuRongo/Bot/rurongobot.py
+++ RuRongo/Bot/rurongobot.py
@@ -4,7 +4,7 @@ import logging
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-from configuration import *
+from configs import *
 import os
 import rurongoscript as rs
 
