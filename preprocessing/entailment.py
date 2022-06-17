318bca520b63cd6a44729fd87964ec125cfae84e
nlptechbook <60931109+nlptechbook@users.noreply.github.com>
2022-06-17 08:56:37 -0400

    Update entailment.py

--- preprocessing/entailment.py
+++ preprocessing/entailment.py
@@ -11,18 +11,26 @@ def cb_entailment(passage):
   for sent in reversed(list(doc.sents)):
     for token in sent:
       if token.dep_ == 'ccomp':
-        ccomp, verb, negation = ccomp_extraction(sent.text)
-        return ccomp, verb, negation
+        ccomp, verb, negation, modal = ccomp_extraction(sent.text)
+        return ccomp, verb, negation, modal
   return
 
 def ccomp_extraction(sent):
   doc = nlp(sent)
   root = [token for token in doc if token.head == token][0]
   ccomp = [token for token in doc if token.dep_ == 'ccomp'][0]
+  verb = ccomp.head
   neg = 'no_negation'
+  if [token for token in verb.lefts if token.dep_ == 'neg']:
+    neg = 'negation'
   if [token for token in root.lefts if token.dep_ == 'neg']:
     neg = 'negation'
+  mod = 'no_modal'
+  if [token for token in verb.lefts if token.dep_ == 'aux' and token.tag_ == 'MD']:
+    mod = 'modal'
+  if [token for token in root.lefts if token.dep_ == 'aux' and token.tag_ == 'MD']:
+    mod = 'modal'    
   ccomp_span = doc[ccomp.left_edge.i: ccomp.right_edge.i + 1]
   if ccomp.left_edge.dep_ in ['mark']:
     ccomp_span = ccomp_span[1:]
-  return ccomp_span.text, root.lemma_, neg
+  return ccomp_span.text, verb.lemma_, neg
